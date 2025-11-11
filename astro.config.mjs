5522e143cd0de8b590149977685e12ae95aa693b
jrojat <jeremy.rojat@gmail.com>
2025-11-11 16:25:13 +0100

    feat(dep): remove sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,9 @@
 import { defineConfig } from 'astro/config';
 
-import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
   devToolbar: {
     enabled: false
   },
-  integrations: [sitemap()]
 });
